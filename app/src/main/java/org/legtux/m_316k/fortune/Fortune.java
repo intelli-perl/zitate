dfbef27f7372a3a7bf95381c2e9ccd9ec85226a9
intelli-perl <chris.ahrend@googlemail.com>
2025-03-30 22:56:28 +0200

    Initial version based on github.com/316k/android-fortune

---
+++ app/src/main/java/org/legtux/m_316k/fortune/Fortune.java
@@ -0,0 +1,174 @@
+package org.legtux.m_316k.fortune;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.AssetManager;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Stack;
+
+
+public class Fortune {
+
+    private int TLDR_LENGTH = 200;
+
+    private String dirname = ""; //CA debug
+    private ArrayList<String> entries;
+    private static Fortune instance;
+    private static Context context;
+    private HashSet<String> categories;
+
+    private int fortune = 0;
+
+    private Random random = null;
+
+    private Stack<Integer> previous = null;
+    private Stack<Integer> next = null;
+
+    public static void setContext(Context context) {
+        Fortune.context = context;
+    }
+
+    public static Fortune instance(Boolean tldr) {
+        if(Fortune.instance == null) {
+            Fortune.instance = new Fortune(tldr);
+        }
+        return Fortune.instance;
+    }
+
+    public Fortune(Boolean tldr) {
+        this.random = new Random();
+        this.entries = new ArrayList<>();
+        this.previous = new Stack<>();
+        this.next = new Stack<>();
+
+        try {
+            AssetManager assets = this.context.getAssets();
+
+            for(String categorie : this.selectedCategories()) {
+                InputStream stream = assets.open("fortunes/" + categorie);
+
+                byte[] b = new byte[stream.available()];
+                stream.read(b);
+
+                String content = new String(b);
+
+                for (String entry : content.split("\n%\n")) {
+                    if(entry.trim().length() > 0) {
+                        this.entries.add(entry.trim());
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            // Gotta catch'em all
+            this.dirname += "|e1:["+e.getMessage()+"|"+e.getCause()+"]"; //CA debug
+            Log.e("Fortune", e.getMessage());
+        }
+
+        File dir = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), "/Download/fortunes");
+        this.dirname += "d:["+dir.toString()+"]"; //CA debug
+
+        try {
+            if(dir.exists()) {
+
+                String[] customFiles = dir.list();
+
+                for (String name : customFiles) {
+                    File f = new File(dir.getAbsolutePath(), name);
+
+                    StringBuilder content = new StringBuilder();
+
+                    BufferedReader br = new BufferedReader(new FileReader(f));
+                    String line;
+
+                    while ((line = br.readLine()) != null) {
+                        content.append(line);
+                        content.append('\n');
+                    }
+
+                    br.close();
+
+                    for (String entry : content.toString().split("\n%\n")) {
+                        if (entry.trim().length() > 0) {
+                            this.entries.add(entry.trim());
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            //You'll need to add proper error handling here
+            // No I don't
+            this.dirname += "|e2:["+e.getMessage()+"|"+e.getCause()+"]"; //CA debug
+            Log.e("Fortune", e.getMessage());
+        }
+
+        this.fortune = this.random.nextInt(this.entries.size());
+        while(tldr && this.current().length() > TLDR_LENGTH)
+            this.fortune = this.random.nextInt(this.entries.size());
+    }
+
+    public String previous() {
+        this.next.push(this.fortune);
+        this.fortune = this.previous.pop();
+        return this.current();
+    }
+
+    public String current() {
+        return this.entries.get(this.fortune);
+
+    }
+    public String next(Boolean tldr) {
+        this.previous.push(this.fortune);
+
+        if(this.next.empty()) {
+            this.fortune = this.random.nextInt(this.entries.size());
+
+            while(tldr && this.current().length() > TLDR_LENGTH)
+                this.fortune = this.random.nextInt(this.entries.size());
+
+        } else {
+            this.fortune = this.next.pop();
+        }
+
+        return this.current();
+    }
+
+    public Boolean previousAvailable() {
+        return !this.previous.empty();
+    }
+    public HashSet<String> allCategories() {
+        AssetManager assets = this.context.getAssets();
+
+        HashSet<String> categories = new HashSet<String>();
+
+        try {
+            for (String file : assets.list("fortunes")) {
+                categories.add(file);
+            }
+        } catch(IOException io) {
+            Log.d("Fortune", io.getMessage());
+        }
+
+        return categories;
+    }
+
+    public HashSet<String> selectedCategories() {
+        SharedPreferences preferences = this.context.getSharedPreferences("general", Context.MODE_PRIVATE);
+        this.categories = (HashSet<String>) preferences.getStringSet("categories", allCategories());
+        return this.categories;
+    }
+
+    public String getDirName() { //CA debug
+        return "Dirname=["+this.dirname+"]"; //CA debug
+    } //CA debug
+}
---
+++ app/src/main/java/org/legtux/m_316k/fortune/MainActivity.java
@@ -0,0 +1,113 @@
+package org.legtux.m_316k.fortune;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Intent;
+
+import static android.R.attr.id;
+
+public class MainActivity extends ActionBarActivity {
+    private Fortune fortune;
+    private TextView fortuneDisplayer;
+    private Button prevFortune;
+    private Button nextFortune;
+    private ShareActionProvider shareActionProvider;
+    private MenuItem tldr;
+    private SharedPreferences preferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.preferences = getApplicationContext().getSharedPreferences("general", Context.MODE_PRIVATE);
+
+        Fortune.setContext(getApplicationContext());
+
+        this.fortuneDisplayer = (TextView) findViewById(R.id.fortuneDisplayer);
+
+        this.fortune = Fortune.instance(preferences.getBoolean("tldr", false));
+        ((TextView) findViewById(R.id.fortuneDisplayer)).setText(fortune.getDirName()+"\n"+this.fortune.current()); //CA debug
+
+        this.nextFortune = (Button) findViewById(R.id.newFortune);
+        nextFortune.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fortuneDisplayer.setText(fortune.getDirName()+"\n"+fortune.next(preferences.getBoolean("tldr", false))); //CA debug
+                prevFortune.setEnabled(true);
+            }
+        });
+
+        this.prevFortune = (Button) findViewById(R.id.prev);
+        prevFortune.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fortuneDisplayer.setText(fortune.getDirName()+"\n"+fortune.previous()); //CA debug
+                prevFortune.setEnabled(fortune.previousAvailable());
+            }
+        });
+        prevFortune.setEnabled(fortune.previousAvailable());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem about = (MenuItem) menu.findItem(R.id.action_about);
+        about.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast.makeText(getApplicationContext(), getString(R.string.about_text), Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
+
+        MenuItem share = (MenuItem) menu.findItem(R.id.menu_item_share);
+        this.shareActionProvider = (ShareActionProvider) (MenuItemCompat.getActionProvider(share));
+        share.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                // Share intent
+                Intent sendIntent = new Intent()
+                        .setAction(Intent.ACTION_SEND)
+                        .putExtra(Intent.EXTRA_TEXT, fortune.current())
+                        .setType("text/plain");
+                startActivity(sendIntent);
+                return true;
+            }
+        });
+
+        this.tldr = (MenuItem) menu.findItem(R.id.menu_tldr);
+        Boolean tldr_checked = preferences.getBoolean("tldr", false);
+        tldr.setChecked(tldr_checked);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.menu_tldr) {
+            Boolean tldr_checked = !preferences.getBoolean("tldr", false);
+
+            SharedPreferences.Editor editor = preferences.edit();
+
+            editor.putBoolean("tldr", tldr_checked);
+
+            editor.commit();
+
+            tldr.setChecked(tldr_checked);
+        }
+
+        return true;
+    }
+}
